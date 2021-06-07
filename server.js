29af33b7cf5b025fd246ca418949842e21b3a32a
Eduardo <eduardo.alvarado1234@ymail.com>
2021-06-07 17:41:34 -0500

    fixing server issues

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ const wallpaperRoutes = require('./api/routes/wallpaperRoutes');
 userRoutes(app);
 wallpaperRoutes(app);
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, './frontend-react/build')));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './frontend-react/build', 'index.html'));
+});
 
 app.use(errorController);
 
